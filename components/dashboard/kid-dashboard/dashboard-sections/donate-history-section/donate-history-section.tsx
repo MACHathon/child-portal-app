7b12f10c08e6af6de7c831b60946e25a17974fdd
Dave Leigh <david.leigh@valtech.com>
2021-09-10 07:59:02 +0100

    get history

--- components/dashboard/kid-dashboard/dashboard-sections/donate-history-section/donate-history-section.tsx
+++ components/dashboard/kid-dashboard/dashboard-sections/donate-history-section/donate-history-section.tsx
@@ -1,16 +1,33 @@
-import ColourSection from "@/components/shared-components/sections/colour-section"
-import { NextPage } from "next"
+import ColourSection from "@/components/shared-components/sections/colour-section";
+import { ProductProjection } from "@commercetools/platform-sdk";
+import { NextPage } from "next";
+import { getChildDonatedHistory } from "packages/Commercetools/Items/getItemInventory";
+import { getMe } from "packages/Commercetools/Users/getUser";
+import { Me } from "packages/Commercetools/Users/Me";
+import React from "react";
+import { useEffect } from "react";
 
 const DonateHistorySection: NextPage = (): JSX.Element => {
-    return (
-        <ColourSection 
-            bgColour='#66B8EC'
-            thumbColor='#ACD9F0'
-            trackColor='#5091BA'
-        >
-            <p>History</p>
-        </ColourSection>
-    )
-}
+  const [me, setMe] = React.useState<Me | null>(null);
+  const [history, setHistory] = React.useState<ProductProjection[]>([]);
 
-export default DonateHistorySection
+  useEffect(() => {
+    (async () => {
+      let me = await getMe();
+      setMe(me);
+      console.log(me);
+      let toysDonated = await getChildDonatedHistory(me?.id as string);
+
+      console.log(toysDonated.body.results);
+      setHistory(toysDonated.body.results);
+    })();
+  }, []);
+
+  return (
+    <ColourSection bgColour="#66B8EC" thumbColor="#ACD9F0" trackColor="#5091BA">
+      <p>History</p>
+    </ColourSection>
+  );
+};
+
+export default DonateHistorySection;
--- packages/Commercetools/Items/getItemInventory.ts
+++ packages/Commercetools/Items/getItemInventory.ts
@@ -1,6 +1,29 @@
-import {  LoggedInUserClient } from "../Clients/APIClient";
+import { LoggedInUserClient } from "../Clients/APIClient";
 
 export const getInventory = async (retailerCommercetoolsId: string) => {
-
-  return await LoggedInUserClient.productProjections().search().get( { queryArgs : { markMatchingVariants: false, filter: 'variants.attributes.assigned-to:"a2f74fcd-7cbc-45f6-b790-e3322bf554f3"'} }).execute();
+  return await LoggedInUserClient.productProjections()
+    .search()
+    .get({
+      queryArgs: {
+        markMatchingVariants: false,
+        filter:
+          'variants.attributes.assigned-to:"a2f74fcd-7cbc-45f6-b790-e3322bf554f3"',
+      },
+    })
+    .execute();
 };
+
+
+export const getChildDonatedHistory = async (childUserId: string) => {
+  return await LoggedInUserClient.productProjections()
+    .search()
+    .get({
+      queryArgs: {
+        markMatchingVariants: false,
+        filter:
+          `variants.attributes.child-id:"${childUserId}"`,
+      },
+    })
+    .execute();
+};
+
