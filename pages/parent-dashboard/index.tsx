98f83a7db9c1b3d310339390e15d450a7816e847
Dave Leigh <david.leigh@valtech.com>
2021-09-14 21:58:45 +0100

    logout parent

--- pages/parent-dashboard/index.tsx
+++ pages/parent-dashboard/index.tsx
@@ -46,6 +46,10 @@ const ParentDashboard: NextPage<Props> = ({ cards }) => {
       }
     );
 
+     // Deleeting cookies... never easy
+     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+
+
     if (rawResponse.status == 200) {
       window.location.href = "/";
     }
